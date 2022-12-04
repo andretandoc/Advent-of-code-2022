1de9eed18ba0c6099ac68443da3960e0b1b0f536
unknown <andrevonvince@gmail.com>
2022-12-04 01:38:55 -0500

    finished day 1 part 2

--- aoc1/main.py
+++ aoc1/main.py
@@ -3,16 +3,25 @@ with open('input.txt') as f:
 
 calorie_count = 0
 max_calorie_count = 0
+second_calorie_count = 0
+third_calorie_count = 0
+
 for item in data:
     if item == '':
-        count = 0
+        calorie_count = 0
     else:       
         calories = int(item)
         calorie_count += calories
         
-
     if calorie_count > max_calorie_count:
+        third_calorie_count = second_calorie_count
+        second_calorie_count = max_calorie_count
         max_calorie_count = calorie_count
+    elif calorie_count > second_calorie_count:
+        second_calorie_count = calorie_count
+    elif calorie_count > third_calorie_count:
+        third_calorie_count = calorie_count
 
-print('The max is:', max_calorie_count)
 
+print('the maximum amount of calories carried by an elf is:', max_calorie_count)
+print('the total amount of calories carried by the top 3 elves is:', max_calorie_count+second_calorie_count+third_calorie_count)
